360f7ae05ed12b1a85c82199b11b250770fde316
SayYoungMan <nickte89@gmail.com>
2023-02-28 20:59:25 +0000

    fix: isXeliable working

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import { isXeliable } from "./src/lib"
 
-console.log(isXeliable([6,5,8,2], 10))
+console.log(isXeliable([6, 5, 8, 2], 10, true))
--- src/lib/isXeliable.ts
+++ src/lib/isXeliable.ts
@@ -3,10 +3,15 @@
  * then returns whether it is possible to produce y from operations between numbers in X.
  * @param X - List of input numbers
  * @param y - Expected Output number
- * 
+ * @param log - Boolean of whether you want the log of X and y or not
+ *
  * @returns 'true' if y can be produced from X, otherwise 'false'
  */
-export function isXeliable(X: number[], y: number): boolean {
+export function isXeliable(X: number[], y: number, log?: boolean): boolean {
+  if (log) {
+    console.log(X, y)
+  }
+
   // If no element in X, then not possible to make y whatsoever.
   if (X.length === 0) {
     return false
@@ -20,15 +25,23 @@ export function isXeliable(X: number[], y: number): boolean {
     return false
   }
 
-  // Otherwise recursively check for all cases (+, -, *, /)
+  // Otherwise recursively check for all cases (+, -, *, /) for all combinations
   // 'popped' will always be number because we have taken care of cases where X.length is 0.
-  const popped = X.pop()! 
-  return (
-    isXeliable(X, y + popped) ||
-    isXeliable(X, y - popped) ||
-    isXeliable(X, popped - y) ||
-    isXeliable(X, y * popped) ||
-    isXeliable(X, y / popped) ||
-    isXeliable(X, popped / y)
-  )
+  for (let i = 0; i < X.length; i++) {
+    const XCopy = [...X]
+    const popped = XCopy.splice(i, 1)[0]!
+    const res =
+      isXeliable([...XCopy], y + popped, log) ||
+      isXeliable([...XCopy], y - popped, log) ||
+      isXeliable([...XCopy], popped - y, log) ||
+      isXeliable([...XCopy], y * popped, log) ||
+      isXeliable([...XCopy], y / popped, log) ||
+      isXeliable([...XCopy], popped / y, log)
+
+    if (res === true) {
+      return true
+    }
+  }
+
+  return false
 }
